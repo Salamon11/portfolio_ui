1f43ff638644b029b0e36b101d72b2a9e0824f50
Salamon11 <salamonpjs2001@gmail.com>
2026-04-03 12:10:04 +0530

    cors fix added

--- src/app/service/contact.service.ts
+++ src/app/service/contact.service.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ContactMessage, ContactResponse } from '../model/contact.model';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  private apiUrl = 'http://localhost:8081/api/contact';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   sendMessage(contact: ContactMessage): Observable<ContactResponse> {
-    return this.http.post<ContactResponse>(this.apiUrl, contact);
+    return this.http.post<ContactResponse>(
+      `${this.baseUrl}/api/contact`,   // ✅ IMPORTANT
+      contact
+    );
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://portfolio-production-eee4.up.railway.app'
+};
